67df403c58b70611b676c699faa8dfe81fa03a78
tontal1705 <63768989+tontal1705@users.noreply.github.com>
2020-12-09 09:39:36 +0700

    add search chart

--- db/database.js
+++ db/database.js
@@ -84,8 +84,8 @@ const getTotalDeath = async() => {
     }
 }
 
-const getLatLong = async() => {
-    const sql = `select "Province/State" as State , "Country/Region" as Country, lat , long from covid19_death_csv`
+const getLatLongCon = async() => {
+    const sql = `select "Province/State" as State , "Country/Region" as Country, lat , long , "3/23/20" as value from covid19_confirmed_csv`
     try {
         const data = await pool.query(sql);
         return data;
@@ -94,8 +94,133 @@ const getLatLong = async() => {
         return null;
     }
 }
-const getThailandConfirmed = async() => {
-    const sql = `select "3/23/20" from covid19_confirmed_csv where "Country/Region" like '%Thailand%'`
+
+//Thailand//
+const getAllCountryTH = async() => {
+    const sql = `SELECT "Province/State" as State , "Country/Region" as Country from covid19_confirmed_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllConfirmedTH = async() => {
+    const sql = `select "3/23/20" as Confirmed from covid19_confirmed_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllRecoveredTH = async() => {
+    const sql = `select "3/23/20" as Confirmed from covid19_recovered_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllDeathTH = async() => {
+    const sql = `select "3/23/20" as Confirmed from covid19_death_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getTotalConfirmTH = async() => {
+    const sql = `select sum("3/23/20") as confirmed from covid19_confirmed_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getTotalRecoveredTH = async() => {
+    const sql = `select sum("3/23/20") as recovered from covid19_recovered_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getTotalDeathTH = async() => {
+    const sql = `select sum("3/23/20") as death from covid19_death_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getLatLongTH = async() => {
+    const sql = `select "Province/State" as State , "Country/Region" as Country, lat , long from covid19_death_csv where "Country/Region" like 'Thailand'`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const Search = async(searchResult) => {
+
+        const sql = `select * from covid19_confirmed_csv ccc where "Country/Region" like '%${searchResult}%'`
+
+        try {
+            const data = await pool.query(sql);
+            return data;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
+    //countries
+const GetAllCountries = async() => {
+    const sql = `select "Province/State" as state, "Country/Region" as country, "3/23/20" as confirmed from covid19_confirmed_csv ccc where "lat" != 15 and "long" != 101 order by "Country/Region", "Province/State" asc`
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const GetAllCountriesRecovered = async() => {
+    const sql = `select "Province/State" as state, "Country/Region" as country, "3/23/20" as confirmed from covid19_recovered_csv ccc where "lat" != 15 and "long" != 101 order by "Country/Region", "Province/State" asc  `
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const GetAllCountriesDeath = async() => {
+    const sql = `select "Province/State" as state, "Country/Region" as country, "3/23/20" as confirmed from covid19_death_csv ccc where "lat" != 15 and "long" != 101 order by "Country/Region", "Province/State" asc`
     try {
         const data = await pool.query(sql);
         return data;
@@ -113,6 +238,21 @@ module.exports = {
     getTotalConfirm,
     getTotalRecovered,
     getTotalDeath,
-    getLatLong,
-    getThailandConfirmed,
+    getLatLongCon,
+
+    getAllCountryTH,
+    getAllConfirmedTH,
+    getAllRecoveredTH,
+    getAllDeathTH,
+    getTotalConfirmTH,
+    getTotalRecoveredTH,
+    getTotalDeathTH,
+    getLatLongTH,
+
+    Search,
+
+    GetAllCountries,
+    GetAllCountriesRecovered,
+    GetAllCountriesDeath,
+
 }
